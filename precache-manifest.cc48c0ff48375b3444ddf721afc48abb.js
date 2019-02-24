5bc5a9c04af2172e415bc209935b0a720811177f
Baltsevich Alexander <baltsevicha@gmail.com>
2019-02-24 14:34:51 +0300

    Updates

---
+++ precache-manifest.cc48c0ff48375b3444ddf721afc48abb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3f7a92089f9c00f3b66272a0b0d0a133",
+    "url": "/accessibility/static/media/accessibility.3f7a9208.jpg"
+  },
+  {
+    "revision": "628afa65a2f3b3993f53",
+    "url": "/accessibility/static/js/runtime~main.628afa65.js"
+  },
+  {
+    "revision": "499d45e20fc23204adc6",
+    "url": "/accessibility/static/js/main.499d45e2.chunk.js"
+  },
+  {
+    "revision": "8c63d51a48c78c832a1a",
+    "url": "/accessibility/static/js/1.8c63d51a.chunk.js"
+  },
+  {
+    "revision": "9317f8cc9651dd6fd07076df31d04697",
+    "url": "/accessibility/index.html"
+  }
+];
